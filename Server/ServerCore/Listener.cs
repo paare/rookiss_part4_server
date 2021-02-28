38ef3fae99195efde44aa1b28521876a632c0db0
sungho <paare@naver.com>
2021-03-01 00:58:10 +0900

    Packet Session 까지 시청 완료 자료

---
+++ Server/ServerCore/Listener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    public class Listener
+    {
+        private Socket _listenSocket;
+
+        private Func<Session> _sessionFactory;
+
+        public void Init(IPEndPoint endPoint, Func<Session> sessionFactory)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _sessionFactory += sessionFactory;
+            _listenSocket.Bind(endPoint);
+
+            // backlog : 최대 대기수
+            _listenSocket.Listen(10);
+
+            var args = new SocketAsyncEventArgs();
+            args.Completed += OnAcceptCompleted;
+            RegisterAccept(args);
+        }
+
+        private void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            var pending = _listenSocket.AcceptAsync(args);
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        private void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                var session = _sessionFactory?.Invoke();
+                session.Start(args.AcceptSocket);
+                session.OnConnected(args.AcceptSocket.RemoteEndPoint);
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            RegisterAccept(args);
+        }
+    }
+}
